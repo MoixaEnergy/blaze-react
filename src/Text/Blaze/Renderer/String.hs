deecc9f354e9ce255be38e54f294a2e939f8333a
Simon Meier <simon.y.meier@gmail.com>
2015-02-19 09:02:50 +0100

    Adapt string renderer to serialization changes.
    Note that I've also dropped support for 'External' for now. I'll re-add it
    later.

--- src/Text/Blaze/Renderer/String.hs
+++ src/Text/Blaze/Renderer/String.hs
@@ -9,10 +9,7 @@ module Text.Blaze.Renderer.String
       renderHtml
     ) where
 
-import qualified Data.ByteString         as S
-import qualified Data.ByteString.Char8   as SBC
 import qualified Data.HashMap.Strict     as HMS
-import           Data.List               (isInfixOf)
 import           Data.Monoid
 import qualified Data.Text               as T
 
@@ -40,17 +37,6 @@ fromChoiceString :: ChoiceString  -- ^ String to render
 fromChoiceString (Static s)     = getString s
 fromChoiceString (String s)     = escapeMarkupEntities s
 fromChoiceString (Text s)       = escapeMarkupEntities $ T.unpack s
-fromChoiceString (ByteString s) = (SBC.unpack s ++)
-fromChoiceString (PreEscaped x) = case x of
-    String s -> (s ++)
-    Text   s -> (\k -> T.foldr (:) k s)
-    s        -> fromChoiceString s
-fromChoiceString (External x) = case x of
-    -- Check that the sequence "</" is *not* in the external data.
-    String s     -> if "</" `isInfixOf` s then id else (s ++)
-    Text   s     -> if "</" `T.isInfixOf` s then id else (\k -> T.foldr (:) k s)
-    ByteString s -> if "</" `S.isInfixOf` s then id else (SBC.unpack s ++)
-    s            -> fromChoiceString s
 fromChoiceString (AppendChoiceString x y) =
     fromChoiceString x . fromChoiceString y
 fromChoiceString EmptyChoiceString = id
@@ -64,30 +50,23 @@ renderString
 renderString =
     go id
   where
-    go :: (String -> String) -> MarkupM ev b -> String -> String
+    go :: (String -> String) -> Markup ev -> String -> String
     go attrs html = case html of
-        MapEvents _f content ->
-            go attrs content
         OnEvent _ev content ->
-            -- TODO (meiersi): add more details about the event handler registered.
             go attrs content
-        Parent _ open close content ->
-            getString open . attrs . ('>' :) . go id content . getString close
-        CustomParent tag content ->
+        Parent tag content ->
             ('<' :) . fromChoiceString tag . attrs . ('>' :) .
             go id content .
             ("</" ++) . fromChoiceString tag . ('>' :)
-        Leaf _ begin end ->
-            getString begin . attrs . getString end
-        CustomLeaf tag close ->
+        Leaf tag close ->
             ('<' :) . fromChoiceString tag . attrs .
             (if close then (" />" ++) else ('>' :))
-        AddAttribute _ key value h ->
-            let attrs' = getString key . fromChoiceString value
+        AddAttribute key value h ->
+            let attrs' = (' ' :) . fromChoiceString key . ("=\"" ++) . fromChoiceString value
                        . ('"' :) . attrs
             in go attrs' h
         AddBoolAttribute key value h ->
-            let attrs' = (' ' :) . getString key . ("=\"" ++)
+            let attrs' = (' ' :) . fromChoiceString key . ("=\"" ++)
                        . ((if value then "true" else "false") ++) .  ('"' :) .  attrs
             in go attrs' h
         AddCustomAttribute key value h ->
@@ -95,7 +74,7 @@ renderString =
                        . fromChoiceString value .  ('"' :) .  attrs
             in go attrs' h
         AddObjectAttribute key object h ->
-            let attrs' = (' ' :) . getString key . ("=\"" ++)
+            let attrs' = (' ' :) . fromChoiceString key . ("=\"" ++)
                        . ((T.unpack $ renderMap object) ++)
             in go attrs' h
         Content content -> fromChoiceString content
